25186b5a1400b6a834d14c407767d8489b6e948b
Brian Feldman <bfeldman@gratii.com>
2016-01-18 10:42:17 -0800

    fixed resubmit

--- backend/createLead.php
+++ backend/createLead.php
@@ -5,11 +5,11 @@ $email = $_POST['email'];
 $lead = getLeadByEmail($email);
 if($lead['lead']){
 
-	insertLead($email);
-	// http_response_code(403);
-	// echo json_encode(array("error"	=> true,
-	// 						"msg"	=> "Looks like you've already signed up for Fixed alerts. ".$email." is already on our list."));
-	// return;
+	resubmitLead($email);
+	http_response_code(403);
+	echo json_encode(array("error"	=> true,
+							"msg"	=> "Looks like you've already signed up for Fixed alerts. ".$email." is already on our list."));
+	return;
 }
 
 $output = insertLead($email);
